3a4770f2506e08e90efaccfb4cda29d0942e6e61
Crowdbotics <team@crowdbotics.com>
2020-11-17 16:20:35 +0000

    Update models

--- backend/find_your_fit_22696/wsgi.py
+++ backend/find_your_fit_22696/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'find_your_fit_22696.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "find_your_fit_22696.settings")
 
 application = get_wsgi_application()
